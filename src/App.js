e297cf0049eb82eaa607c3f809d406658ed4ebb3
Don-Stevenson <dsteven1@gmail.com>
2020-04-01 17:24:17 -0400

    display date

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ const api = {
 };
 
 function App() {
+  const dateBuilder = (d) => {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[d.getDay()]
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return  `${day} ${date} ${month} ${year}`
+  }
+  <div className="app">
   return (
-    const dateBuilder = (date) => {
-      const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-      const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    }
-    <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." />
