cb22a0739f207053a6342b9b48b878930c00bbd2
Don-Stevenson <dsteven1@gmail.com>
2020-04-01 17:19:33 -0400

    set up date and location function

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ const api = {
 
 function App() {
   return (
+    const dateBuilder = (date) => {
+      const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+      const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    }
     <div className="app">
       <main>
         <div className="search-box">
-          <input type="text" className="search-box" placeholder="Search..." />
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, Us</div>
+          <div className="date">{dateBuilder(new Date())} </div>
+           
         </div>
       </main>
     </div>
