29c4ac071609cbcf23fcf581de4f6772ad5f53ee
Don-Stevenson <dsteven1@gmail.com>
2021-02-06 17:12:07 -0500

    update header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
-    return (
-        <div>
-            Header here
-        </div>
-    )
+  return (
+    <div className="HeadingTop">
+      <h1>The Weather App</h1>
+    </div>
+  );
 }
